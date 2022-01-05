c716d6943dd75438834973b6328c4d2620b83502
oishikatta90 <xxxwon90@gmail.com>
2022-01-05 18:12:01 +0900

    나머지 쿼리 어노테이션으로 마이바티스랑 연동해봤음

--- src/main/java/com/lee/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/lee/exam/demo/repository/ArticleRepository.java
@@ -2,22 +2,29 @@ package com.lee.exam.demo.repository;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.lee.exam.demo.vo.Article;
 
 @Mapper
 public interface ArticleRepository {
+	@Insert("insert into article set regDate=now(), updateDate=now(), title= #{title}, `body`= #{body}")
 	public Article writeArticle(String title, String body);
 	
 	@Select("select * from article where id = #{id}")
 	public Article getArticle(@Param("id") int id);
 
+	@Select("select * from article order by id desc")
 	public List<Article> getArticles();
-
-	public void deleteArticle(int id);
-
+	
+	@Delete("delete from article where id = #{id}")
+	public void deleteArticle(@Param("id")int id);
+	
+	@Update("update article set title = #{title}, `body` =#{body} where id = #{id}")
 	public void modifyArticle(int id, String title, String body);
 }
