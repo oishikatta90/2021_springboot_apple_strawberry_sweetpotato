4706f7d6c4a1bbcc65ea932b93539ab8f252858e
oishikatta90 <xxxwon90@gmail.com>
2022-01-05 12:43:20 +0900

    count 조절 액션 구현

--- src/main/java/com/lee/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/lee/exam/demo/controller/UsrHomeController.java
@@ -9,34 +9,18 @@ public class UsrHomeController {
 	private int count;
 	
 	public UsrHomeController() {
-		count = 0;
+		count = -1;
 	}
-	@RequestMapping("/usr/home/main")
+
+	@RequestMapping("/usr/home/getCount")
 	@ResponseBody
-	public String showMain() {
-		return "안녕하세요";
-	}
-	@RequestMapping("/usr/home/main2")
-	@ResponseBody
-	public String showMain2() {
-		return "반갑습니다";
-	}
-	@RequestMapping("/usr/home/main3")
-	@ResponseBody
-	public String showMain3() {
-		return "또 만나요";
-	}
-	
-	@RequestMapping("/usr/home/main4")
-	@ResponseBody
-	public int showMain4() {
-		count++;
+	public int getCount() {
 		return count;
 	}
-	@RequestMapping("/usr/home/main5")
+	@RequestMapping("/usr/home/doSetCount")
 	@ResponseBody
-	public String showMain5() {
-		count = 0;
-		return "카운터의 값이 0으로 초기화 되었습니다.";
+	public String doSetCount(int count) {
+		this.count = count;
+		return "카운터의 값이 "+ this.count + "으로 초기화 되었습니다.";
 	}
 }
