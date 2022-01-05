ec9b8b7da3f0a51d416b79674b4993c6073606d2
oishikatta90 <xxxwon90@gmail.com>
2022-01-05 11:25:11 +0900

    세팅

---
+++ src/test/java/com/lee/exam/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lee.exam.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
