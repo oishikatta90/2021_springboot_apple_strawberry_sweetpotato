bd80d7467dc070237d2ca1efe58072a2f8575191
oishikatta90 <xxxwon90@gmail.com>
2022-01-05 18:55:37 +0900

    sql 어노테이션 애들을 xml파일을 만들어서 거기로 다시 옮겨서 수정

--- src/main/java/com/lee/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/lee/exam/demo/repository/ArticleRepository.java
@@ -2,32 +2,22 @@ package com.lee.exam.demo.repository;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
 
 import com.lee.exam.demo.vo.Article;
 
 @Mapper
 public interface ArticleRepository {
-	@Insert("insert into article set regDate=now(), updateDate=now(), title= #{title}, `body`= #{body}")
-	public void writeArticle(@Param("title")String title, @Param("body")String body);
-	
-	@Select("select * from article where id = #{id}")
+	public void writeArticle(@Param("title") String title, @Param("body") String body);
+
 	public Article getArticle(@Param("id") int id);
 
-	@Select("select * from article order by id desc")
 	public List<Article> getArticles();
-	
-	@Select("select last_insert_id()")
+
 	public int getLastInsertId();
-	
-	@Delete("delete from article where id = #{id}")
-	public void deleteArticle(@Param("id")int id);
-	
-	@Update("update article set title = #{title}, `body` =#{body} where id = #{id}")
+
+	public void deleteArticle(@Param("id") int id);
+
 	public void modifyArticle(int id, String title, String body);
 }
