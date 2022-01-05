98844b0a89de7dd1a2ef252ee2354774751b2732
oishikatta90 <xxxwon90@gmail.com>
2022-01-05 14:49:38 +0900

    게시물 수정

--- src/main/java/com/lee/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/lee/exam/demo/controller/UsrArticleController.java
@@ -58,6 +58,17 @@ public class UsrArticleController {
 		articles.remove(article);
 		
 	}
+	
+	private void modifyArticle(int id, String title, String body) {
+		Article article = getArticle(id);
+		
+		article.setTitle(title);
+		article.setBody(body);
+		
+		
+		
+	}
+	
 	//서비스 메서드 끝
 	
 	//ActionMethod 시작점
@@ -86,6 +97,19 @@ public class UsrArticleController {
 		
 		deleteArticle(id);
 		return id + "번 게시물이 삭제되었습니다.";
+		
+	}
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody
+	public String doModify(int id, String title, String body) {
+		Article article = getArticle(id);
+		
+		if(article == null) {
+			return id + "번 게시물이 존재하지 않습니다.";
+		}
+		
+		modifyArticle(id, title, body);
+		return id + "번 게시물이 수정되었습니다.";
 		
 	}
 	//ActionMethod 끝
