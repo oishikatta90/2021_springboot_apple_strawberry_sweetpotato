01f600bd5a9ce14794357178b53c27cc25b8c481
oishikatta90 <xxxwon90@gmail.com>
2022-01-05 13:39:30 +0900

    테스트용 게시물 10개 생성:

--- src/main/java/com/lee/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/lee/exam/demo/controller/UsrArticleController.java
@@ -15,8 +15,22 @@ public class UsrArticleController {
 	public UsrArticleController() {
 		articlesLastId = 0;
 		articles = new ArrayList<>();
+		
+		makeTestData();
 	}
 	
+	private void makeTestData() {
+		for (int i = 1; i <= 10; i++) {
+			int id = articlesLastId + 1;
+			String title = "제목" + i;
+			String body = "내용" + i;
+			Article article = new Article(id, title, body);
+			
+			articles.add(article);
+			articlesLastId = id;
+		}
+	}
+
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
@@ -33,6 +47,13 @@ public class UsrArticleController {
 	@ResponseBody
 	public List<Article> getArticles(String title, String body) {
 		return articles;
+		
+	}
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody
+	public String doDelete(int id) {
+		articles.remove(id);
+		return id + 1 + "번 게시물이 삭제되었습니다.";
 		
 	}
 }
