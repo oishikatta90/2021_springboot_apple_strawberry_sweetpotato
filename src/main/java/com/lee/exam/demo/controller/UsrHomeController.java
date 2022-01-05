e8a6e461158ac04016f8dfa0b319980ce5552933
oishikatta90 <xxxwon90@gmail.com>
2022-01-05 15:19:48 +0900

    ArticleController에 있던 서비스 로직을 ArticleService로 옮김

--- src/main/java/com/lee/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/lee/exam/demo/controller/UsrHomeController.java
@@ -1,82 +1,16 @@
 package com.lee.exam.demo.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 @Controller
 public class UsrHomeController {
-	@RequestMapping("/usr/home/getString")
+	
+	@RequestMapping("/usr/home/main")
 	@ResponseBody
 	public String getString() {
 		return "HI";
 	}
-	@RequestMapping("/usr/home/getInt")
-	@ResponseBody
-	public int getInt() {
-		return 10;
-	}
-	@RequestMapping("/usr/home/getFloat")
-	@ResponseBody
-	public float getFloat() {
-		return 10.5f;
-	}
-	@RequestMapping("/usr/home/getDouble")
-	@ResponseBody
-	public double getDouble() {
-		return 10.5;
-	}
-	@RequestMapping("/usr/home/getBoolean")
-	@ResponseBody
-	public boolean getBoolean() {
-		return false;
-	}
-	@RequestMapping("/usr/home/getChar")
-	@ResponseBody
-	public char getChar() {
-		return 'a';
-	}
-	@RequestMapping("/usr/home/getMap")
-	@ResponseBody
-	public Map<String, Object> getMap() {
-		Map<String, Object> map = new HashMap<>();
-		map.put("철수나이", 22);
-		map.put("영희나이", 21);
-		return map;
-	}
-	@RequestMapping("/usr/home/getList")
-	@ResponseBody
-	public List<String> getList() {
-		List<String> list = new ArrayList<>();
-		list.add("철수는 22살");
-		list.add("영희는 21살");
-		return list;
-	}
-	@RequestMapping("/usr/home/getArticles")
-	@ResponseBody
-	public List<Article> getArticles() {
-		
-		List<Article>articles = new ArrayList<Article>();
-		
-		return articles;
-	}
-}
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
- class Article {
-	 private int id;
-	 private String title;
-	 private String body;
-	 
- }
+}
--- src/main/java/com/lee/exam/demo/service/ArticleService.java
+++ src/main/java/com/lee/exam/demo/service/ArticleService.java
@@ -1,5 +1,70 @@
 package com.lee.exam.demo.service;
 
-public class ArticleService {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.lee.exam.demo.vo.Article;
+
+
+
+@Service
+public class ArticleService {
+	private List<Article> articles;
+	private int articlesLastId;
+ 
+	public ArticleService() {
+		articlesLastId = 0;
+		articles = new ArrayList<>();
+		
+		makeTestData();
+	}
+
+	private void makeTestData() {
+		for (int i = 1; i <= 10; i++) {
+			String title = "제목" + i;
+			String body = "내용" + i;
+
+			writeArticle(title, body);
+		}
+	}
+
+	public Article writeArticle(String title, String body) {
+		int id = articlesLastId + 1;
+		Article article = new Article(id, title, body);
+
+		articles.add(article);
+		articlesLastId = id;
+
+		return article;
+	}
+
+	public Article getArticle(int id) {
+		for (Article article : articles) {
+			if (article.getId() == id) {
+				return article;
+			}
+		}
+
+		return null;
+	}
+	
+	public List<Article> getArticles() {
+		return articles;
+	}
+
+	public void deleteArticle(int id) {
+		Article article = getArticle(id);
+
+		articles.remove(article);
+
+	}
+
+	public void modifyArticle(int id, String title, String body) {
+		Article article = getArticle(id);
+
+		article.setTitle(title);
+		article.setBody(body);
+	}
 }
