b92ca4eecc23c4527779caf09ae66de5b632eae0
oishikatta90 <xxxwon90@gmail.com>
2022-01-05 13:09:06 +0900

    액션메서드에 각종 리턴타입 실험

--- src/main/java/com/lee/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/lee/exam/demo/controller/UsrHomeController.java
@@ -1,26 +1,92 @@
 package com.lee.exam.demo.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Controller
 public class UsrHomeController {
-	private int count;
-	
-	public UsrHomeController() {
-		count = -1;
-	}
-
-	@RequestMapping("/usr/home/getCount")
+	@RequestMapping("/usr/home/getString")
 	@ResponseBody
-	public int getCount() {
-		return count;
+	public String getString() {
+		return "HI";
 	}
-	@RequestMapping("/usr/home/doSetCount")
+	@RequestMapping("/usr/home/getInt")
 	@ResponseBody
-	public String doSetCount(int count) {
-		this.count = count;
-		return "카운터의 값이 "+ this.count + "으로 초기화 되었습니다.";
+	public int getInt() {
+		return 10;
+	}
+	@RequestMapping("/usr/home/getFloat")
+	@ResponseBody
+	public float getFloat() {
+		return 10.5f;
+	}
+	@RequestMapping("/usr/home/getDouble")
+	@ResponseBody
+	public double getDouble() {
+		return 10.5;
+	}
+	@RequestMapping("/usr/home/getBoolean")
+	@ResponseBody
+	public boolean getBoolean() {
+		return false;
+	}
+	@RequestMapping("/usr/home/getChar")
+	@ResponseBody
+	public char getChar() {
+		return 'a';
+	}
+	@RequestMapping("/usr/home/getMap")
+	@ResponseBody
+	public Map<String, Object> getMap() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("철수나이", 22);
+		map.put("영희나이", 21);
+		return map;
+	}
+	@RequestMapping("/usr/home/getList")
+	@ResponseBody
+	public List<String> getList() {
+		List<String> list = new ArrayList<>();
+		list.add("철수는 22살");
+		list.add("영희는 21살");
+		return list;
+	}
+	@RequestMapping("/usr/home/getArticle")
+	@ResponseBody
+	public Article getArticle() {
+		Article article = new Article(1, "제목1");
+		
+		return article;
+	}
+	@RequestMapping("/usr/home/getArticles")
+	@ResponseBody
+	public List<Article> getArticles() {
+		Article article1 = new Article(1, "제목1");
+		Article article2 = new Article(2, "제목2");
+		
+		List<Article>articles = new ArrayList<Article>();
+		articles.add(article1);
+		articles.add(article2);
+		
+		return articles;
 	}
 }
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+ class Article {
+	 private int id;
+	 private String title;
+	 
+ }
