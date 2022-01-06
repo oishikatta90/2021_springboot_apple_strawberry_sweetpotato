7f76cfec0fe370adb785238c1be0844acb81df54
oishikatta90 <xxxwon90@gmail.com>
2022-01-06 15:09:31 +0900

    Ut.f 함수 도입으로 복잡한 문장 구성을 편하게

--- src/main/java/com/lee/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/lee/exam/demo/controller/UsrMemberController.java
@@ -41,11 +41,11 @@ public class UsrMemberController {
 		System.out.println(id);
 		
 		if (id == -1) {
-			return "이미 등록된 회원 아이디입니다.";
+			return Ut.f("해당 로그인아이디(%s)는 이미 사용중입니다.", loginId);
 		}
 		
 		if (id == -2) {
-			return "이미 등록된 이름과 이메일입니다.";
+			return Ut.f("해당 이름(%s)과 이메일(%s)는 이미 사용중입니다.", name, email);
 		}
 		Member member = memberService.getMemberById(id);
 		
