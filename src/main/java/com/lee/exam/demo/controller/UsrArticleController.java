6055e6b8b065b7124e1dc6387a62f7af9cc13764
oishikatta90 <xxxwon90@gmail.com>
2022-01-06 15:42:50 +0900

    getArticle에 ResultData에서 만들어 놓은 양식을 적용

--- src/main/java/com/lee/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/lee/exam/demo/controller/UsrArticleController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.lee.exam.demo.service.ArticleService;
+import com.lee.exam.demo.util.Ut;
 import com.lee.exam.demo.vo.Article;
+import com.lee.exam.demo.vo.ResultData;
 
 
 @Controller
@@ -40,9 +42,10 @@ public class UsrArticleController {
 		Article article = articleService.getArticle(id);
 		
 		if (article == null) {
-			return id + "번 게시물은 존재하지 않습니다.";
+			
+			return ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id));
 		}
-		return article;
+		return ResultData.from("S-1", Ut.f("%d번 게시물입니다.", id));
 		
 	}
 	@RequestMapping("/usr/article/doDelete")
