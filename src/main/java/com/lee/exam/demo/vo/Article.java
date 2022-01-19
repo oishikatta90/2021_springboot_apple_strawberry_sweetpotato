d660a73e5891647f33994ec871ee5b216fafbdf2
oishikatta90 <xxxwon90@gmail.com>
2022-01-19 13:56:02 +0900

    jsp에서 시간을 출력하던 부분을 article에 메소드를 추가해 거기서 시간을 정렬하고 jsp에서 아티클에 메소드만 불러서 시간이 표시되게 변경

--- src/main/java/com/lee/exam/demo/vo/Article.java
+++ src/main/java/com/lee/exam/demo/vo/Article.java
@@ -20,4 +20,20 @@ public class Article {
 	private String extra__writerName;
 	private boolean extra__actorCanDelete;
 	private boolean extra__actorCanModify;
+	
+	public String getForPrintType1RegDate() {
+		return regDate.substring(2, 16).replace(" ", "<br>");
+	}
+
+	public String getForPrintType1UpdateDate() {
+		return regDate.substring(2, 16).replace(" ", "<br>");
+	}
+
+	public String getForPrintType2RegDate() {
+		return regDate.substring(2, 16);
+	}
+	
+	public String getForPrintType2UpdateDate() {
+		return regDate.substring(2, 16);
+	}
 }
