7f799ceecf98a7b203a5ff970fba9de40362b1bb
oishikatta90 <xxxwon90@gmail.com>
2022-02-04 20:17:30 +0900

    로그인 후 접근 할 수 없는 페이지에서는 afterLoginUri없이 로그인 페이지로 이동

--- src/main/java/com/lee/exam/demo/vo/Rq.java
+++ src/main/java/com/lee/exam/demo/vo/Rq.java
@@ -1,6 +1,7 @@
 package com.lee.exam.demo.vo;
 
 import java.io.IOException;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -28,10 +29,13 @@ public class Rq {
 	private HttpServletRequest req;
 	private HttpServletResponse resp;
 	private HttpSession httpSession;
+	private Map<String, String>paramMap;
 	
 	public Rq(HttpServletRequest req, HttpServletResponse resp, MemberService memberService) {
 		this.req = req;
 		this.resp = resp;
+		
+		paramMap = Ut.getParamMap(req);
 		
 		this.httpSession = req.getSession();
 		
@@ -156,6 +160,16 @@ public class Rq {
 	}
 
 	public String getAfterLoginUri() {
+		String requestUri = req.getRequestURI();
+
+		switch (requestUri) {
+		case "/usr/member/login":
+		case "/usr/member/join":
+		case "/usr/member/findLoginId":
+		case "/usr/member/findLoginPw":
+			return Ut.getUriEncoded(paramMap.get("afterLoginUri"));
+		}
+
 		return getEncodedCurrentUri();
 	}
 
